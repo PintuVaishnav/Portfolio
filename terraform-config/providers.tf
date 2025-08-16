84ee427c3e77b75c8051463a5cec630ace6c7636
PintuVaishnav <vaishnavyejju@gmail.com>
2025-08-16 20:23:56 +0530

    Added Argocd

--- terraform-config/providers.tf
+++ terraform-config/providers.tf
@@ -19,3 +19,13 @@ provider "google" {
   region  = "us-central1"
   zone    = "us-central1-a"
 }
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${google_container_cluster.primary.endpoint}"
+    token                  = data.google_client_config.default.access_token
+    cluster_ca_certificate = base64decode(google_container_cluster.primary.master_auth[0].cluster_ca_certificate)
+    config_path = "~/.kube/config"
+  }
+  
+}
